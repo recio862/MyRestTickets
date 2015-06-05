d696f6112fa827ba6c8caad620604ad66a7400de
recio862 <recio862@utexas.edu>
2015-06-05 18:10:44 -0500

    url typo

--- app/urlservices.py
+++ app/urlservices.py
@@ -2,20 +2,20 @@ __author__ = 'rjr862'
 import string
 
 base_url = "http://127.0.0.1:5000/"
-ticket_url = '/projects/<int:project>/tickets/<int:ticket>'
-tickets_url = '/projects/<int:project>/tickets'
-project_url = '/projects/<int:project>'
-projects_url = '/projects/'
+ticket_url = 'projects/<int:project>/tickets/<int:ticket>'
+tickets_url = 'projects/<int:project>/tickets'
+project_url = 'projects/<int:project>'
+projects_url = 'projects/'
 
 
 def get_ticket_url(project, ticket):
     result = base_url + ticket_url
-    result = string.replace(result, '<int:project>', project)
-    result = string.replace(result, '<int:ticket>', ticket)
+    result = result.replace('<int:project>', str(project))
+    result = result.replace('<int:ticket>', str(ticket))
     return result
 
 
 def get_project_url(project):
     result = base_url + project_url
-    result = string.replace(project_url, '<int:project>', project)
+    result = result.replace('<int:project>', str(project))
     return result
